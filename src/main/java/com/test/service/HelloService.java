6220543fbe1ed4b096ec3207d34651bd58356790
Administrator <Administrator@SC-201704032046>
2018-08-20 22:41:18 +0800

    springmvc annotation test

---
+++ src/main/java/com/test/service/HelloService.java
@@ -0,0 +1,13 @@
+package com.test.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloService {
+	
+	public String sayHello(String name){
+		
+		return "Hello "+name;
+	}
+
+}
